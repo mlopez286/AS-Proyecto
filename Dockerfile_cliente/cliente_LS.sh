219284d938835e10aa4f3787ad444a324c0444aa
mlopez286 <90972425+mlopez286@users.noreply.github.com>
2022-11-27 15:25:53 +0100

    Dockerfiles[OK] + Docker-compose[OK]
    La aplicación funciona con docker-compose.yml

--- Dockerfile_cliente/cliente_LS.sh
+++ Dockerfile_cliente/cliente_LS.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-# 2. Suscribirse al item chat_room
+# Variables
+#-------------------------
 aux=/data/infoSesion.txt
 ip="192.168.0.2:8080"
 
+# Hace un poco de tiempo para que sesion-ls pueda crear la sesión
+sleep 20 
+# Conseguir el ID de sesión almacenado en /data/infoSesion.txt
 sesionID=$(cat $aux | head -n 1 | cut -d ',' -f 2) 
+
+# Suscribirse al item "chat_room"
 curl -v -N -X POST -d "LS_op=add&LS_subId=1&LS_data_adapter=CHAT_ROOM&LS_group=chat_room&LS_schema=timestamp message&LS_mode=DISTINCT&LS_session=$sesionID&LS_reqId=1" $ip/lightstreamer/control.txt?LS_protocol=TLCP-2.0.0
 
 
-# 3. Publicar mensajes en los campos timestamp y message del item chat_room
-
+# Publicar 100 mensajes en el item "chat_room"(timestamp, message). Se enviarán 100 mensajes con la forma "Mensaje: X" siendo X un número aleatorio.
+# Esperará 1s entre mensajes.
 lista=$(seq {1,100})
 mensaje=""
-echo "Se enviarán 100 mensajes..."
+echo ""
+echo "####################################################################"
+echo "#                         ENVÍO DE MENSAJES                        #"
+echo "####################################################################"
 
+echo "Se enviarán 100 mensajes al servidor Lightstreamer..."
+echo ""
 for i in $lista
 do
+	
 	mensaje=$(echo "Mensaje: $RANDOM")
+	echo "	ENVIANDO... > $mensaje"
         curl -v -N -X POST -d "LS_session=$sesionID&LS_message=CHAT|$mensaje&LS_outcome=false&LS_reqId=1" $ip/lightstreamer/msg.txt?LS_protocol=TLCP-2.0.0
 	sleep 1 
 
--- Dockerfile_sesion/sesion_LS.sh
+++ Dockerfile_sesion/sesion_LS.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# 1. Crear la sesión y guardar información en infoSesion.txt
-aux=/data/infoSesion.txt
-#ip=http://localhost:8080
+# 1. Crear la sesión y guardar información en sesionInfo.txt
+info=/data/infoSesion.txt
 
+#ip=http://localhost:8080
 ip="192.168.0.2:8080"
-curl -o $aux -v -N -X POST -d "LS_adapter_set=CHAT&LS_cid=mgQkwtwdysogQz2BJ4Ji%20kOj2Bg" $ip/lightstreamer/create_session.txt?LS_protocol=TLCP-2.0.0 
-# Conseguir el ID de la sesión recien creada
-sesionID=$(cat $aux | head -n 1 | cut -d ',' -f 2)
-echo $sesionID > $aux
+
+
+sleep 10
+echo ""
+echo "####################################################################"
+echo "#                         SESIÓN Y MENSAJES                        #"
+echo "####################################################################"
+echo ""
+echo "Este cliente creará un ID de sesión y se quedará a la escucha de mensajes
+del servidor Lightstreamer.Todos los mensajes de la sesión se guardarán en /data/infoSesion.txt"
+echo "-------------------------------------------------------------------------"
+echo ""
+
+# Crear una sesión utilizando el set de adaptadores "Chat" y volcar el output al fichero sesionInfo.txt que se alojará en un volumen
+curl -o $info -v -N -X POST -d "LS_adapter_set=CHAT&LS_cid=mgQkwtwdysogQz2BJ4Ji%20kOj2Bg" $ip/lightstreamer/create_session.txt?LS_protocol=TLCP-2.0.0 
